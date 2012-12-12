55a7a592bcf1a5a7903696e0ce660df233223f1d
Nero <237326369@163.com>
2012-12-12 11:54:46 +0800

    c
    czx

---
+++ build/TestPad.build/Debug-iphoneos/TestPad.build/Objects-normal/armv7/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: /Users/user/Desktop/资料/TestPad/TestPad/AppDelegate.m \
+  /Users/user/Desktop/资料/TestPad/TestPad/AppDelegate.h \
+  /Users/user/Desktop/资料/TestPad/TestPad/ViewController.h
---
+++ build/TestPad.build/Debug-iphoneos/TestPad.build/Objects-normal/armv7/AsyncSocket.d
@@ -0,0 +1,2 @@
+dependencies: /Users/user/Desktop/资料/TestPad/AsyncSocket.m \
+  /Users/user/Desktop/资料/TestPad/AsyncSocket.h
